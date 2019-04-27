fe4b59daa4b96b42e8aa0340a27e8eae0d8d3b2d
Jinto Jose <jintoppy@gmail.com>
2019-04-27 14:45:49 +0530

    controlled component form

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+class AddUser extends Component {
+    state = {
+        name: '',
+        age: ''
+    };
+    onBtnClick = (e) => {
+        e.preventDefault();
+        if(this.state.name){
+            this.props.onAddUser({
+                name: this.state.name,
+                age: this.state.age
+            });
+        }
+    };
+    onChange = (evnt) => {
+        const { name, value } = evnt.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
-const AddUser = () => {
-    
+    render() {
+        return (
+            <form>
+                <div>
+                    <label>Name</label>
+                    <input 
+                        type="text"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.onChange}
+                    />   
+                </div>
+                <div>
+                    <label>Age</label>
+                    <input 
+                        type="number"
+                        name="age"
+                        value={this.state.age}
+                        onChange={this.onChange}
+                    />   
+                </div>
+                <div>
+                    <button onClick={this.onBtnClick}>Add User</button>
+                </div>
+            </form>
+        )
+    }
 };
 
 export default AddUser;
